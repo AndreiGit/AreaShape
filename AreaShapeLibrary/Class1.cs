d9d4ccc760573c0eeb945a6d9696833f6cbd0a46
KhramovAndrey <104138811+KhramovAndrey@users.noreply.github.com>
2023-05-02 10:34:22 +0400

    add shape base

--- AreaShapeLibrary/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AreaShapeLibrary
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ AreaShapeLibrary/ShapeBase.cs
@@ -0,0 +1,14 @@
+﻿namespace AreaShapeLibrary
+{
+    public abstract class ShapeBase
+    {
+        public string Name { get; private set; }
+
+        public ShapeBase(string shapeName)
+        {
+            Name = shapeName;
+        }
+
+        public abstract float CalculateArea();
+    }
+}
